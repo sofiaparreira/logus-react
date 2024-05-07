92254c53bb57df5355f2f14f969887fde8c1bada
Sofia <sofia@MacBook-Pro-Sofia.local>
2024-05-06 21:37:39 -0300

    Desing do TextInput Search

--- src/Pages/Dashboard.tsx
+++ src/Pages/Dashboard.tsx
@@ -1,28 +1,35 @@
 import React from "react";
 import { VStack, View, Image, Text, Box, HStack, Input } from "native-base";
-import { TouchableOpacity, TextInput  } from "react-native";
+import { TouchableOpacity, TextInput } from "react-native";
 import MenuIcon from '../assets/menuIcon.png';
 import ProfileIcon from '../assets/profileIcon.png'
 
 
 export default function Dashboard() {
     return (
-        <View bg='purple.500' paddingTop={8}>
+        <View bg='purple.500' paddingTop={16}>
             <VStack paddingX={4} h={32} justifyContent="flex-start">
                 <Box flex={1} flexDirection={'row'} justifyContent={'space-between'} marginBottom={'16px'}>
                     <TouchableOpacity>
                         <Image w={12} h={12} source={MenuIcon} />
                     </TouchableOpacity>
 
-                    <Image w={12} h={12} source={ProfileIcon} />
+                    <Image w={16} h={16} source={ProfileIcon} />
                 </Box>
                 <Text marginBottom={4} fontSize="4xl" fontWeight={"black"} color={'white'}>Olá, User</Text>
             </VStack>
-            <HStack w={'100%'} h={'100%'} paddingTop={8} paddingX={4} bg='white' borderTopRadius={32}>
-            <TextInput
-                    style={{ flex: 1, paddingLeft: 8 }}
-                    placeholder="Pesquisar"
+            <HStack w={'100%'} bg='white' borderTopRadius={32}>
+                <TextInput
+                    style={{ 
+                        flex: 1, paddingLeft: 24, paddingVertical: 24, 
+                        borderRadius: 28, borderWidth: 2, borderColor: 'gray' ,
+                        fontSize: 16
+                        
+                    }}
+                    
+                    placeholder="Pesquise por resumos, atividades, slides ..."
                 />
+
             </HStack>
         </View>
     );
